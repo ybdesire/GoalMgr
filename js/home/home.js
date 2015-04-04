4549c20b477c949b174866aee57e81efe59696f5
ybdesire <ybdesire@gmail.com>
2015-04-04 16:05:53 +0800

    Add list view dynamically.

--- js/home/home.js
+++ js/home/home.js
@@ -1,4 +1,16 @@
 $("document").ready(function () {
+    //init home page
+    (function () {
+        if(window.localStorage.getItem("goalCount")===null)//no goals
+        {
+            window.localStorage.setItem("goalCount", "0");
+        }
+        else if (parseInt(window.localStorage.getItem("goalCount"))>0)
+        {
+            loadGoalsFromLocalStorage();
+        }
+    })();
+
     $("#editBtn").click(function () {
         if ($("#editBtn").html() === "Edit")
         {
@@ -26,6 +38,31 @@ $("document").ready(function () {
                         </div>");
         $("div[data-role='navbar']").navbar();
 
+        //add footer navbar click function
+        $("#addGoal").click(function () {
+            var goalSeq = localStorage.getItem("goalCount");
+            var liId = "goal" + goalSeq;
+            var goalTitle = "goal" + goalSeq;
+            var goalTitleId = "goal" + goalSeq + "Title";
+            var goalCompleteId = "goal" + goalSeq + "Complete";
+            var goalComplete0 = "0% completed";
+
+            var listHtml = "<li id='" + liId + "'>" +
+                                "<a href='#'>" +
+                                    "<h2 id='" + goalTitleId + "'>" + goalTitle + "</h2>" +
+                                    "<p id='" + goalCompleteId + "'>" + goalComplete0 + "</p>" +
+                                "</a>" +
+                            "</li>";
+
+            $("#goalsList").append(listHtml);
+            //increase goal count
+            var newGoalSeq = parseInt(goalSeq) + 1;
+            localStorage.setItem("goalCount", String(newGoalSeq));
+
+            $('ul').listview('refresh');
+
+        });
+
     }
 
     function removeFooter()
@@ -33,7 +70,9 @@ $("document").ready(function () {
         $("#footerDiv").remove();
     }
 
-    $("#addGoal").click(function () {
 
-    });
+    function loadGoalsFromLocalStorage()
+    {
+
+    }
 });
