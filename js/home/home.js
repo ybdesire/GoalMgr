b63f35e52dbde38accfed95b797e9ea2fd9bbce9
ybdesire <ybdesire@gmail.com>
2015-04-04 14:25:53 +0800

    Add/remove footer bar by 'edit' button

--- js/home/home.js
+++ js/home/home.js
@@ -1,13 +1,39 @@
 $("document").ready(function () {
     $("#editBtn").click(function () {
-        addFooter();
+        if ($("#editBtn").html() === "Edit")
+        {
+            $("#editBtn").html("Done");
+            addFooter();
+        }
+        else
+        {
+            $("#editBtn").html("Edit");
+            removeFooter();
+        }
+        
     });
-
-
 
     function addFooter()
     {
-        //$("#homePageFooter").attr("data-role", "footer");
-        $("#homePageFooter").append("<h1>asdf</h1>");
+        $("#homePageFooter").append("<div id='footerDiv'></div>");//Add div to hold navbar
+        //add navbar to footer
+        $("#footerDiv").append(" \
+                        <div data-role='navbar'>    \
+                            <ul>                    \
+                                <li><a href='#' data-icon='minus' id='rmGoal'></a></li>            \
+                                <li><a href='#' data-icon='plus' id='addGoal'></a></li>             \
+                            </ul>                                                                   \
+                        </div>");
+        $("div[data-role='navbar']").navbar();
+
     }
+
+    function removeFooter()
+    {
+        $("#footerDiv").remove();
+    }
+
+    $("#addGoal").click(function () {
+
+    });
 });
